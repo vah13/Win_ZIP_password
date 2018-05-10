70c24ea25649d751bf031a92285b51ae7573dd6a
vah_13 <vvvaagn@gmail.com>
2018-05-10 17:55:10 +0300

    upd

--- main.py
+++ main.py
@@ -13,16 +13,3 @@ script = session.create_script(source)
 script.on('message', on_message)
 script.load()
 sys.stdin.read()
-```
-hook.js
-```
-var hndl;
-hndl = Module.findExportByName("ShLwApi.dll", "SHUnicodeToAnsi"); 
-//find  SHUnicodeToAnsi function from  ShLwApi.dll DLL
-// hooked function has this view - SHUnicodeToAnsi("password",0x11bedee4,260) , where  args[0] = password
-Interceptor.attach(hndl, {
-    onEnter: function(args) {
-        console.log("[!] Hooked SHUnicodeToAnsi function")
-        console.log( Memory.readUtf16String(args[0])) // getting password
-    }
-});
